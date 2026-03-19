222bee4c24e6023279b880f508cd298421b6631d
Arseniy Petrenko <arsfix2004@gmail.com>
2026-03-19 23:32:37 +0200

    Fixed Solution 3

--- app/test_main.py
+++ app/test_main.py
@@ -2,14 +2,26 @@ from app.main import is_isogram
 
 
 def test_is_isogram() -> None:
-    assert is_isogram("playgrounds")
-    assert is_isogram("")
-    assert is_isogram("hi")
-    assert is_isogram("word")
-    assert is_isogram("play")
-    assert is_isogram("run")
-    assert is_isogram("man")
-    assert is_isogram("woman")
-    assert is_isogram("python")
-    assert is_isogram("ark")
-    assert is_isogram("night")
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("") is True
+    assert is_isogram("hi") is True
+    assert is_isogram("word") is True
+    assert is_isogram("play") is True
+    assert is_isogram("run") is True
+    assert is_isogram("man") is True
+    assert is_isogram("woman") is True
+    assert is_isogram("python") is True
+    assert is_isogram("ark") is True
+    assert is_isogram("night") is True
+
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
+    assert is_isogram("hell") is False
+    assert is_isogram("Hello") is False
+    assert is_isogram("test") is False
+    assert is_isogram("Heroes") is False
+    assert is_isogram("darkness") is False
+    assert is_isogram("papa") is False
+    assert is_isogram("mama") is False
+    assert is_isogram("pip") is False
+    assert is_isogram("digital") is False
