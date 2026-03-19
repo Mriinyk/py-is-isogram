c4ea4a75b4a67a853c285f71ea1fba316d33b2e0
Arseniy Petrenko <arsfix2004@gmail.com>
2026-03-19 23:17:03 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram() -> None:
+    assert is_isogram("Aa")
+    assert is_isogram("Bb")
+    assert is_isogram("Cc")
+    assert is_isogram("hell")
+    assert is_isogram("Hello")
+    assert is_isogram("hi")
+    assert is_isogram("word")
+    assert is_isogram("play")
+    assert is_isogram("run")
+    assert is_isogram("test")
+    assert is_isogram("Heroes")
+    assert is_isogram("darkness")
+    assert is_isogram("man")
+    assert is_isogram("woman")
+    assert is_isogram("papa")
+    assert is_isogram("mama")
+    assert is_isogram("pip")
+    assert is_isogram("python")
+    assert is_isogram("digital")
